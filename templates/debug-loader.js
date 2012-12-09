4ad48aa22e96b387c91fc89b16a08e3605ceb90c
Tim Schaub <tim.schaub@gmail.com>
2012-12-08 17:53:05 -0700

    Lint free

--- templates/debug-loader.js
+++ templates/debug-loader.js
@@ -1,15 +1,12 @@
-(function() {
-    var files = {{scripts}};
-
-    var scripts = document.getElementsByTagName("script");
-    var index = scripts[scripts.length-1].src;
-
-    var pieces = new Array(scripts.length);
-
-    var src;
-    for (var i=0; i<files.length; i++) {
-        src = index + "/" + files[i];
-        pieces[i] = "<script src='" + src + "'></script>";
-    }
-    document.write(pieces.join(""));
-})();
+(function(d) {
+  var files = {{scripts}},
+      scripts = d.getElementsByTagName('script'),
+      index = scripts[scripts.length - 1].src,
+      pieces = new Array(scripts.length),
+      src, i;
+  for (i = 0; i < files.length; ++i) {
+    src = index + '/' + files[i];
+    pieces[i] = '<script src="' + src + '"></script>';
+  }
+  d.write(pieces.join(''));
+})(document);
